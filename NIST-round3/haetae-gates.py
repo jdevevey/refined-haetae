974026a4511970b53b6439187380561968d5dbcb
Julien Devevey <julien.devevey@ens-lyon.fr>
2023-12-22 17:15:10 +0100

    level II and III computations done

--- NIST-round3/haetae-gates.py
+++ NIST-round3/haetae-gates.py
@@ -3,26 +3,18 @@ from cost import *
 
 # Result of the DDGR20 BKZ prediction
 #
-# ======= Dilithium-NL2 round3
-# GSA Intersect:               dim=2049   δ=1.003828      β=423.43 
-# Probabilistic simulation:    dim=2049                   β=433.73
+# ======= HAETAE-120
+# GSA Intersect:               dim=1281   δ=1.003788      β=429.76 
+# Probabilistic simulation:    dim=1281                   β=439.01
 # ======= Dilithium-NL3 round3
-# GSA Intersect:               dim=2654   δ=1.002903      β=623.61
-# Probabilistic simulation:    dim=2654                   β=638.33
+# GSA Intersect:               dim=2049   δ=1.002384      β=814.54
+# Probabilistic simulation:    dim=2049                   β=834.76
 # ======= Dilithium-NL5 round3
 # GSA Intersect:               dim=3540   δ=1.002284      β=862.84  
 # Probabilistic simulation:    dim=3540                   β=883.56 
 
-# As expected, the GSA intersect prediction roughly match the 
-# one made by the dilithium dedicated script :
-#
-# Dilithium-NL2  : Primal attacks uses block-size 423 and 1010 samples
-# Dilithium-NL3  : Primal attacks uses block-size 624 and 1285 samples
-# Dilithium-NL5  : Primal attacks uses block-size 863 and 1670 samples
-
-
 print("              \t& n   \t&  β \t& β' \t& gates \t& memory ")
-print("Dilithium-NL2 \t& %d\t& %d\t& %d\t& %.1f \t& %.1f "%summary(2049, 433))
-print("Dilithium-NL3 \t& %d\t& %d\t& %d\t& %.1f \t& %.1f "%summary(2654, 638))
-print("Dilithium-NL5 \t& %d\t& %d\t& %d\t& %.1f \t& %.1f "%summary(3540, 883))
+print("HAETAE-120 \t& %d\t& %d\t& %d\t& %.1f \t& %.1f "%summary(1281, 439))
+print("HAETAE-180 \t& %d\t& %d\t& %d\t& %.1f \t& %.1f "%summary(2049, 834))
+#print("Dilithium-NL5 \t& %d\t& %d\t& %d\t& %.1f \t& %.1f "%summary(3540, 883))
 
